ac1fb09ff17711a806058adea0dd7e427a22e1cc
ctrlzaurus <ichi.di@icloud.com>
2023-04-04 01:27:53 +0300

    dattebayo

--- src/services/pixabay.js
+++ src/services/pixabay.js
@@ -20,7 +20,7 @@
 
 const searchPixabayApi = async (query, page) => {
   const response = await fetch(
-    `https://pixabay.com/api/?key=34227427-bed69196739ab14a9612e550e&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=40`
+    `https://pixabay.com/api/?key=34227427-bed69196739ab14a9612e550e&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
   )
   .then((response) => response.json())
   return response;
